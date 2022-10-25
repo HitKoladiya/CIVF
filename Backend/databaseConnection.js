054e3ec1814a24afa82e8b797b0fbbb159c95bf9
HitKoladiya <104366639+HitKoladiya@users.noreply.github.com>
2022-10-25 15:45:43 +0530

    Update databaseConnection.js

--- Backend/databaseConnection.js
+++ Backend/databaseConnection.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect("mongodb+srv://hitkoladiya:CIVF@cluster0.wzsvd5c.mongodb.net/test");
+mongoose.connect("mongodb url");
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'hii'));
 db.once('open',()=>{
